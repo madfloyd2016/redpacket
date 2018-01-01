c724d86395dc55d881f07504f10f3554b2ca1a4a
hackrole <daipeng123456@gmail.com>
2018-01-01 10:42:41 +0800

    add migrate from mariadb

--- redpacket/settings.py
+++ redpacket/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'redpacket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'redpacket',
+        'USER': 'redpacket',
+        'PASSWORD': 'redpacket',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
